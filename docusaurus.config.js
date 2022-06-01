97b8eb4b6938d4efaa1496f4d5ddd590430cb9c3
Dylan <duanyytop@gmail.com>
2022-06-01 16:47:50 +0800

    refactor: Update docs menus and submenus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,51 +1,46 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '秘宝文档中心',
-  tagline: '秘宝——',
-  url: 'https://open.mibao.net',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.png',
-  organizationName: 'nervina-labs', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  title: "秘宝文档中心",
+  tagline: "秘宝——",
+  url: "https://open.mibao.net",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.png",
+  organizationName: "nervina-labs", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
   i18n: {
-    defaultLocale: 'zh',
-    locales: ['en', 'zh'],
+    defaultLocale: "zh",
+    locales: ["en", "zh"],
     localeConfigs: {
-      en: {
-
-      },
+      en: {},
       // You can omit a locale (e.g. fr) if you don't need to override the defaults
-      zh: {
-
-      },
+      zh: {},
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          editUrl: "https://github.com/facebook/docusaurus/edit/main/website/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+          editUrl: "https://github.com/facebook/docusaurus/edit/main/website/blog/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -55,36 +50,36 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: '秘宝文档中心',
+        title: "秘宝文档中心",
         logo: {
-          alt: '秘宝文档中心',
-          src: 'img/logo.png',
+          alt: "秘宝文档中心",
+          src: "img/logo.png",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'about/what-is-new',
-            position: 'left',
-            label: 'Docs',
+            type: "doc",
+            docId: "about/what-is-new",
+            position: "left",
+            label: "Docs",
           },
-          //   {
-          //   label: "Translate",
-          //   position: 'right',
-          //   items: [
-          //     {
-          //       label: '中文',
-          //       to: 'docs/about/what-is-new',
-          //     },
-          //     {
-          //       label: 'English',
-          //       to: 'docs/docs_en/about/what-is-new',
-          //     },
-          //   ],
-          // },
+          {
+            label: "Language",
+            position: "right",
+            items: [
+              {
+                label: "中文",
+                to: "docs/about/what-is-new",
+              },
+              {
+                label: "English",
+                to: "docs/faq",
+              },
+            ],
+          },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         copyright: `Copyright © ${new Date().getFullYear()} Nervina Labs`,
       },
       prism: {
